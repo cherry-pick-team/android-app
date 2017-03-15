8814e2c885b68727e3e3fab39724c0253ab183f2
SchadkoAO <schadko.ao@gmail.com>
2017-03-15 23:35:48 +0300

    add trends saga, connect trends componenet to store, mock api response

--- index.android.js
+++ index.android.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import {AppRegistry, Navigator} from 'react-native';
 import Router from './lib/utils/Router';
 import { ThemeProvider } from 'react-native-material-ui';
+import { Provider } from 'react-redux';
+import { configureStore } from './lib/store'
 
 
 export default class Root extends Component {
 	render() {
 		return (
-			<ThemeProvider>
-				<Navigator
-					initialRoute={{id: 'main'}}
-					renderScene={Router}
-				/>
-			</ThemeProvider>
+			<Provider store={configureStore()}>
+				<ThemeProvider>
+					<Navigator
+						initialRoute={{id: 'main'}}
+						renderScene={Router}
+					/>
+				</ThemeProvider>
+			</Provider>
 		);
 	}
 }
