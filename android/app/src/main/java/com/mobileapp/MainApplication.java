8d9a63ca8148112e8659dfa67b9e47aea7dac4f9
SchadkoAO <schadko.ao@gmail.com>
2017-04-30 14:44:29 +0300

    WIP

--- android/app/src/main/java/com/mobileapp/MainApplication.java
+++ android/app/src/main/java/com/mobileapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.mediaPlayer.ReactNativeAudioStreamingPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new LinearGradientPackage(),
             new ReactNativeAudioStreamingPackage(),
             new ReactMaterialKitPackage(),
             new VectorIconsPackage(),
